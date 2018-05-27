13d0d0260fbcb6a70a8e570279c345bb52f12216
onion <hanxiaoweb163@.com>
2018-05-27 21:45:51 +0800

    修复bug

--- js/process.js
+++ js/process.js
@@ -56,6 +56,7 @@
         var stopTime = new Date().getTime();
         lastPercent = lastPercent + (stopTime - startTime) / (curDuration * 1000)
         cancelAnimationFrame(frameId)
+        console.log(lastPercent)
     }
     // 将计算的时长放入html中
     function randerAllTime(duration) {
